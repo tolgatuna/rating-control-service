78486c29821e9f9b436f6b01a66c48960fdf5142
tolgatuna <tolga.tuna@hotmail.com>
2019-10-12 21:07:50 +0100

    -> RestTemplate changed with RestAssured.given (more useful) and tests are organized again.

--- src/test/java/com/duriancodes/ratingcontrolservice/acceptance/glue/RatingControlLevelStepDefinition.java
+++ src/test/java/com/duriancodes/ratingcontrolservice/acceptance/glue/RatingControlLevelStepDefinition.java
@@ -2,35 +2,32 @@ package com.duriancodes.ratingcontrolservice.acceptance.glue;
 
 import com.duriancodes.ratingcontrolservice.RatingControlServiceApiApplication;
 import com.duriancodes.ratingcontrolservice.acceptance.fixtures.BookServiceFixture;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import io.cucumber.java8.En;
-import org.junit.Assert;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles("local")
 @AutoConfigureWireMock(port = 9999)
-@SpringBootTest(classes = RatingControlServiceApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = RatingControlServiceApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class RatingControlLevelStepDefinition implements En {
 
     private String customerSetControlLevel;
-    private ResponseEntity<Boolean> responseEntity;
-    @Autowired
-    private TestRestTemplate restTemplate;
+    private Response response;
 
     public RatingControlLevelStepDefinition() {
         Given("^I am customer who have set rating control level (.*)$", (String customerControlLevel) -> {
@@ -39,32 +36,40 @@ public class RatingControlLevelStepDefinition implements En {
 
         When("^I request to read equal level book (.*)$", (String bookId) -> {
             BookServiceFixture.stubBookServiceResponseForBook_B1234_Rating12(bookId);
-            HttpEntity httpEntity = new HttpEntity(generateHeader());
-            responseEntity = restTemplate.exchange("/rcl/book/v1/read/eligibility/" + this.customerSetControlLevel + "/" + bookId,
-                    HttpMethod.GET, httpEntity, Boolean.class);
+            response = given()
+                    .headers(generateHeader())
+                    .when()
+                    .get("/rating-test-control/rcl/book/v1/read/eligibility/" + this.customerSetControlLevel + "/" + bookId);
         });
 
         Then("^I get decision to read the book$", () -> {
-            assertThat("responseEntity status code is not 200", responseEntity.getStatusCode(), is(HttpStatus.OK));
-            assertThat("responseEntity body is not true", responseEntity.getBody(), is(Boolean.TRUE));
+            WireMock.verify(WireMock.getRequestedFor(WireMock.urlEqualTo("/fetch/book/rating/B1234")));
+            response.then()
+                    .assertThat()
+                    .statusCode(HttpStatus.OK.value())
+                    .body(is(Boolean.TRUE.toString()));
         });
 
         When("I request to read higher level book (.*)$", (String bookId) -> {
             BookServiceFixture.stubBookServiceResponseForBook_BH1234_Rating15(bookId);
-            HttpEntity httpEntity = new HttpEntity(generateHeader());
-            responseEntity = restTemplate.exchange("/rcl/book/v1/read/eligibility/" + this.customerSetControlLevel + "/" + bookId,
-                    HttpMethod.GET, httpEntity, Boolean.class);
+            response = given()
+                    .headers(generateHeader())
+                    .when()
+                    .get("/rating-test-control/rcl/book/v1/read/eligibility/" + this.customerSetControlLevel + "/" + bookId);
         });
 
         Then("I get decision not to read the book", () -> {
-            Assert.assertThat("responseEntity status code is not 200", responseEntity.getStatusCodeValue(), is(200));
-            Assert.assertThat("Book Read Eligibility is true", responseEntity.getBody(), is(Boolean.FALSE));
+            WireMock.verify(WireMock.getRequestedFor(WireMock.urlEqualTo("/fetch/book/rating/BH1234")));
+            response.then()
+                    .assertThat()
+                    .statusCode(HttpStatus.OK.value())
+                    .body(is(Boolean.FALSE.toString()));
         });
     }
 
-    private MultiValueMap<String, String> generateHeader() {
-        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-        headers.add("Accept", "application/json");
-        return headers;
+    private Headers generateHeader() {
+        List<Header> headers = new ArrayList<>();
+        headers.add(new Header("Accept", "application/json"));
+        return new Headers(headers);
     }
 }
